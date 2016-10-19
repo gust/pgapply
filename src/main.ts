4c21ce9d8c7b372de2eea78e90609cf55b4e9c6b
Bert Hartmann <BertHartm@gmail.com>
2016-10-19 09:18:01 -0400

    add a dumb "install" using pg-promise

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,36 @@
-console.log('hello world');
+const pgp = require('pg-promise')();
+const db = pgp({
+    host: 'localhost',
+    port: 5432,
+    database: 'postgres',
+    user: 'postgres',
+    password: 'password',
+    application_name: 'deploy'
+});
+
+function main() {
+    switch (process.argv[2]) {
+        case 'install':
+            console.log('installing');
+            return db.query('CREATE SCHEMA deploy;').then(function() {
+                console.log('complete');
+            }).catch(function(err: Error) {
+                console.log(err);
+            });
+        default:
+            console.log('did not recognize:', process.argv[0]);
+            console.log('valid actions are:\n' +
+            'install: install into a running postgres instance');
+            return Promise.resolve();
+    }
+}
+
+function shutdown() {
+    pgp.end();
+}
+
+main().catch(function(err: Error) {
+    console.error(err);
+}).then(function() {
+    shutdown();
+})
