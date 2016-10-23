5d65381cf980046aa7bdbf4d8d58be84f30a79fc
Bert Hartmann <BertHartm@gmail.com>
2016-10-23 18:07:52 -0400

    remove a level of promise

--- src/main.ts
+++ src/main.ts
@@ -30,13 +30,27 @@ function main():Promise<any> {
             });
         case 'build-db':
             console.log('building current DB from ', db_file_location);
+            let instance_id:string;
             return new Promise(function(resolve, reject) {
-                let instance_id:string;
+                // start docker image of PG_VERSION
+                // docker run -d -P -e POSTGRES_PASSWORD=password postgres:9.2
+                const child = execFile('docker',
+                    ['run', '-d', '-P', '-e', 'POSTGRES_PASSWORD=password', 'postgres:9.2'],
+                    {},
+                    function(err: Error, stdout:string, stderr:string) {
+                        if (err) {
+                            console.error(stderr);
+                            reject(err);
+                        } else {
+                            resolve(stdout.trim());
+                        }
+                    });
+            }).then(function(iid:string) {
+                instance_id = iid;
+                // retrieve docker host:port
                 return new Promise(function(resolve, reject) {
-                    // start docker image of PG_VERSION
-                    // docker run -d -P -e POSTGRES_PASSWORD=password postgres:9.2
                     const child = execFile('docker',
-                        ['run', '-d', '-P', '-e', 'POSTGRES_PASSWORD=password', 'postgres:9.2'],
+                        ['port', instance_id, '5432/tcp'],
                         {},
                         function(err: Error, stdout:string, stderr:string) {
                             if (err) {
@@ -46,82 +60,66 @@ function main():Promise<any> {
                                 resolve(stdout.trim());
                             }
                         });
-                }).then(function(iid:string) {
-                    instance_id = iid;
-                    // retrieve docker host:port
-                    return new Promise(function(resolve, reject) {
-                        const child = execFile('docker',
-                            ['port', instance_id, '5432/tcp'],
-                            {},
-                            function(err: Error, stdout:string, stderr:string) {
-                                if (err) {
-                                    console.error(stderr);
-                                    reject(err);
-                                } else {
-                                    resolve(stdout.trim());
-                                }
-                            });
+                });
+            }).then(function(host_port:string) {
+                // poll socket for readability
+                let [host, port] = host_port.split(':');
+                if (host == '0.0.0.0') {
+                    host = 'localhost';
+                }
+                const db = pgp({
+                    host,
+                    port: parseFloat(port),
+                    database: 'postgres',
+                    user: 'postgres',
+                    password: 'password',
+                    application_name: 'deploy'
+                });
+                let attempt_connect = function(depth = 0):Promise<void> {
+                    return db.query('select 1;')
+                    .catch(function(err:Error) {
+                        // TODO: only retry if err is ECONNRESET
+                        // TODO: add a delay before trying again
+                        if (depth < 20000) {
+                            return attempt_connect(depth + 1);
+                        } else {
+                            throw err;
+                        }
                     });
-                }).then(function(host_port:string) {
-                    // poll socket for readability
-                    let [host, port] = host_port.split(':');
-                    if (host == '0.0.0.0') {
-                        host = 'localhost';
-                    }
-                    const db = pgp({
-                        host,
-                        port: parseFloat(port),
-                        database: 'postgres',
-                        user: 'postgres',
-                        password: 'password',
-                        application_name: 'deploy'
-                    });
-                    let attempt_connect = function(depth = 0):Promise<void> {
-                        return db.query('select 1;')
-                        .catch(function(err:Error) {
-                            // TODO: only retry if err is ECONNRESET
-                            // TODO: add a delay before trying again
-                            if (depth < 20000) {
-                                return attempt_connect(depth + 1);
+                }
+                return attempt_connect();
+            }).then(function() {
+                // get file(s?) from GIT
+                // dump into pg
+            }).then(function() {
+                // shut down docker image
+                return new Promise(function(resolve, reject) {
+                    const child = execFile('docker',
+                        ['stop', instance_id],
+                        {},
+                        function(err: Error, stdout:string, stderr:string) {
+                            if (err) {
+                                console.error(stderr);
+                                reject(err);
                             } else {
-                                throw err;
+                                resolve(stdout.trim());
                             }
                         });
-                    }
-                    return attempt_connect();
-                }).then(function() {
-                    // get file(s?) from GIT
-                    // dump into pg
-                }).then(function() {
-                    // shut down docker image
-                    return new Promise(function(resolve, reject) {
-                        const child = execFile('docker',
-                            ['stop', instance_id],
-                            {},
-                            function(err: Error, stdout:string, stderr:string) {
-                                if (err) {
-                                    console.error(stderr);
-                                    reject(err);
-                                } else {
-                                    resolve(stdout.trim());
-                                }
-                            });
-                    });
-                }).then(function() {
-                    // remove the instance
-                    return new Promise(function(resolve, reject) {
-                        const child = execFile('docker',
-                            ['rm', instance_id],
-                            {},
-                            function(err: Error, stdout:string, stderr:string) {
-                                if (err) {
-                                    console.error(stderr);
-                                    reject(err);
-                                } else {
-                                    resolve(stdout.trim());
-                                }
-                            });
-                    });
+                });
+            }).then(function() {
+                // remove the instance
+                return new Promise(function(resolve, reject) {
+                    const child = execFile('docker',
+                        ['rm', instance_id],
+                        {},
+                        function(err: Error, stdout:string, stderr:string) {
+                            if (err) {
+                                console.error(stderr);
+                                reject(err);
+                            } else {
+                                resolve(stdout.trim());
+                            }
+                        });
                 });
             });
         default:
@@ -139,5 +137,6 @@ function shutdown() {
 main().catch(function(err: Error) {
     console.error(err);
 }).then(function() {
+    console.log('success');
     shutdown();
 })
