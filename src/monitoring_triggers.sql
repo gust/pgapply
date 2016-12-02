386261e35d9259e615f6a5ac253fe8cfb757ba84
Bert Hartmann <BertHartm@gmail.com>
2016-12-01 20:42:20 -0500

    rough proof of concept of actually determining what changed in a ddl command

--- src/monitoring_triggers.sql
+++ src/monitoring_triggers.sql
@@ -10,14 +10,47 @@ create table deploy.monitoring (
 
 -- see https://www.postgresql.org/docs/9.3/static/event-trigger-definition.html
 
-CREATE OR REPLACE FUNCTION deploy.monitor_proc() RETURNS EVENT_TRIGGER LANGUAGE PLPGSQL AS $$
+CREATE OR REPLACE FUNCTION deploy.monitor_start() RETURNS EVENT_TRIGGER LANGUAGE PLPGSQL AS $$
+DECLARE
+BEGIN
+    -- TG_EVENT = 'ddl_command_start'
+    CASE split_part(TG_TAG, ' ', 2)
+        WHEN 'FUNCTION' THEN
+            DROP TABLE IF EXISTS funcs;
+            CREATE TEMP TABLE funcs AS
+                SELECT * FROM pg_catalog.pg_proc;
+        ELSE
+    END CASE;
+END $$;
+CREATE EVENT TRIGGER ddl_monitor_start
+    ON ddl_command_start
+    EXECUTE PROCEDURE deploy.monitor_start();
+
+CREATE OR REPLACE FUNCTION deploy.monitor_end() RETURNS EVENT_TRIGGER LANGUAGE PLPGSQL AS $$
 DECLARE
     filename varchar;
+    type varchar;
+    name varchar;
+    schema varchar;
 BEGIN
-    SELECT setting INTO filename FROM pg_catalog.pg_settings WHERE name = 'application_name';
+    -- TG_EVENT = 'ddl_command_end'
+    CASE split_part(TG_TAG, ' ', 2)
+        WHEN 'FUNCTION' THEN
+            SELECT a.proname, pgn.nspname INTO name, schema
+                FROM (TABLE pg_catalog.pg_proc EXCEPT TABLE funcs) a
+                LEFT JOIN pg_catalog.pg_namespace pgn ON (a.pronamespace = pgn.oid)
+                LIMIT 1;
+            type = 'function';
+            DROP TABLE funcs;
+        ELSE
+            type = 'not sure';
+            name  = 'not sure';
+            schema = 'not sure';
+    END CASE;
+    SELECT setting INTO filename FROM pg_catalog.pg_settings pgs WHERE pgs.name = 'application_name';
     INSERT into deploy.monitoring (file, type, schema, name, op) VALUES
-        (filename, TG_EVENT, 'not sure', 'not sure', TG_TAG);
+        (filename, type, schema, name, TG_TAG);
 END $$;
-CREATE EVENT TRIGGER monitor_proc
-    ON ddl_command_start
-    EXECUTE PROCEDURE deploy.monitor_proc();
+CREATE EVENT TRIGGER ddl_monitor_end
+    ON ddl_command_end
+    EXECUTE PROCEDURE deploy.monitor_end();
